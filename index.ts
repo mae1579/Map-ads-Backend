47348853d65a1a159f933b42f2485900ca3de1f4
mae1579 <mateuszumiecki@gmail.com>
2022-09-05 15:07:32 +0200

    Upload first version Map-ads back-end
    Signed-off-by: Mateusz Umięcki <mateuszumiecki@gmail.com>

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, {json} from "express";
+import cors from 'cors';
+import 'express-async-errors';
+import {handleError} from "./utils/errors";
+import rateLimit from 'express-rate-limit'
+import {adRouter} from "./routers/ad.router";
+import {config} from "./config/config";
+
+const app = express();
+
+app.use(cors({
+    origin: config.corsOrigin,
+}));
+app.use(json());
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
+
+// const router = Router();
+
+app.use('/ad', adRouter);
+
+// app.use('/api', router);
+
+app.use(handleError);
+
+app.listen(3001, 'localhost', () => {
+    console.log('Listening on port http://localhost:3001');
+});
